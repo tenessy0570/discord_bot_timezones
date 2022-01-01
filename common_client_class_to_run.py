f754c362c72d534054e60ea03daebaf9051cdb85
tenessy0570 <romikp30757@gmail.com>
2022-01-01 22:19:05 +0700

    renamed reactions.py

--- common_client_class_to_run.py
+++ common_client_class_to_run.py
@@ -1,8 +1,7 @@
-from reactions import Reactions
 from discord import Client as ClientToInherit
 from on_messages import Messages
 from on_ready import Ready
-from reactions import Reactions
+from on_reaction import Reactions
 
 
 class Client(ClientToInherit, Ready, Messages, Reactions):
