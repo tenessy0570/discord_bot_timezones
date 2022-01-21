561e03a3aaa8770e03673ef857888080263d347f
tenessy0570 <romikp30757@gmail.com>
2022-01-21 07:29:15 +0700

    using python-dotenv  instead of environ

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
-import environment_vars
-from os import environ
+from os import getenv
+from dotenv import load_dotenv
 from discord import Intents
 from common_client_class_to_run import Client
 
-bot_token = environ.get('bot_token')
+load_dotenv()
+BOT_TOKEN = getenv("BOT_TOKEN")
 
 client = Client(intents=Intents.all())
-client.run(bot_token)
+client.run(BOT_TOKEN)
