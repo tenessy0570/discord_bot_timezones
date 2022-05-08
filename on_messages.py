04bd547271e17c59aa64cd406a8f76a36799798c
tenessy0570 <romikp30757@gmail.com>
2022-05-09 02:42:37 +0700

    test commit

--- on_messages.py
+++ on_messages.py
@@ -12,4 +12,5 @@ class Messages:
             return
 
         if message.content.startswith('!time'):
+            await message.channel.send("Отправляю время...")
             await send_timezones_to_channel(message)
